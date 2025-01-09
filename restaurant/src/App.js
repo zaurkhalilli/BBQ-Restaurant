63182f3d6884357d5c37ddb3e1082f24f1cd4145
Zaur Khalilli <khalillizaur1@gmail.com>
2025-01-09 15:40:28 +0400

    bootstrap added

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,8 +1,9 @@
   import React from 'react'
   const App = () => {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowseRouter>
+      </BrowseRouter>
 
     </div>
   );
---
+++ restaurant/src/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "../Navbar/Navbar.scss";
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            
+        </nav>
+    )
+}
+
+export default Navbar
